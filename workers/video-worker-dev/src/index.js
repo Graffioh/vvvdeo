b6b95ec6d2124366d1b4f9450ee89b5495321310
Graffioh <bregliascuola2002@gmail.com>
2024-12-02 12:48:04 +0100

    Move video-worker in workers, Create video-worker-dev for development env

---
+++ workers/video-worker-dev/src/index.js
@@ -0,0 +1,49 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch() {
+		return new Response('vvvdeo video worker (DEV)');
+	},
+	async queue(batch, env) {
+		for (const message of batch.messages) {
+			const key = message.body.object.key;
+			try {
+				if (key.includes('videos/')) {
+					const videoUploadResponse = await fetch('https://6593-2-45-237-19.ngrok-free.app' + '/video-upload-complete', {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({ videoKey: key, status: 'uploaded' }),
+					});
+
+					if (!videoUploadResponse.ok) {
+						throw new Error(`Failed to send Video upload notification: ${videoUploadResponse.statusText}`);
+					}
+				} else if (key.includes('frames/')) {
+					const frameExtractionResponse = await fetch('https://6593-2-45-237-19.ngrok-free.app' + '/frames-extraction-complete', {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({ videoKey: key, status: 'extracted' }),
+					});
+
+					if (!frameExtractionResponse.ok) {
+						throw new Error(`Failed to send Frames extraction notification: ${frameExtractionResponse.statusText}`);
+					}
+				}
+
+				message.ack();
+			} catch (err) {
+				console.error('Error processing message from queue:', err);
+				console.error('BACKEND URL: ', env.BACKEND_URL);
+				message.retry();
+			}
+		}
+	},
+};
