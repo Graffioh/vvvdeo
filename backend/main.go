f5d78c5a5918059abc67e6bbeb7880fb2af150ac
Graffioh <bregliascuola2002@gmail.com>
2024-12-19 11:58:11 +0100

    Adjust backend cors policies

--- backend/main.go
+++ backend/main.go
@@ -36,7 +36,7 @@ func setupServerHandler() http.Handler {
 	}
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:9000", "https://vvvdeo.pages.dev", "https://vvvdeo.com", "http://localhost:5173", "http://localhost:5174"},
+		AllowedOrigins: []string{"http://localhost:9000", "https://vvvdeo.pages.dev", "https://vvvdeo.com", "http://localhost:5173", "http://localhost:5174", "https://vvvdeo.com/api"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{
 			"Accept",
