9349b926f875d3066300973d9541a0d314c15412
Graffioh <bregliascuola2002@gmail.com>
2024-12-19 14:08:17 +0100

    Add X-Accel header for server sent events

--- backend/events/ffmpeg.go
+++ backend/events/ffmpeg.go
@@ -17,6 +17,7 @@ func FfmpegEventsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("X-Accel-Buffering", "no")
 
 	for msg := range ch {
 		content := fmt.Sprintf("data: %s\n\n", msg)
