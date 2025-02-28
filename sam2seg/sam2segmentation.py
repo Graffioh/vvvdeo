b2b5cc1b05a9b165b11e1f526ec008095f1faef8
Graffioh <bregliascuola2002@gmail.com>
2025-02-28 09:31:24 +0100

    Add check and fallbacks for cpu/cuda/mps devices in segmentation backend

--- sam2seg/sam2segmentation.py
+++ sam2seg/sam2segmentation.py
@@ -1,4 +1,5 @@
 import os
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 import json
 import numpy as np
 import torch
@@ -31,8 +32,27 @@ app.logger.addHandler(log_handler)
 app.logger.setLevel(logging.DEBUG)
 app.logger.info("Starting the application...")
 
-# useless because i can only use the cpu :)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"using device: {device}")
+
+if device.type == "cuda":
+    # use bfloat16 for the entire notebook
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+elif device.type == "mps":
+    print(
+        "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+        "give numerically different outputs and sometimes degraded performance on MPS. "
+        "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+    )
 
 # local model
 #sam2_checkpoint = "./src/sam-2/checkpoints/sam2.1_hiera_small.pt"
